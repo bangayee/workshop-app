0e79db2ee456f2c5e02b74e8b44bfb1aff337e65
Azis Rahmanto <bangayee@gmail.com>
2025-03-27 11:38:03 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,21 +8,18 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
+    
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        Setting::factory()->create([
+        Setting::create([
             'setting_name' => 'logo',
             'value' => '1742982177_logo.png',
             'type' => 'image',
             'status' => 1,
         ]);
 
-        Setting::factory()->create([
+        Setting::create([
             'setting_name' => 'site_name',
             'value' => 'Workshop App',
             'type' => 'text',
